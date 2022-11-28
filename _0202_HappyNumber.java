122413382e60f93ff5870509370198a8fd1141aa
jerrypigtan <80908530+jerrypigtan@users.noreply.github.com>
2022-11-29 00:17:39 +0800

    Add files via upload
    add 0202 happy number

---
+++ _0202_HappyNumber.java
@@ -0,0 +1,49 @@
+package Test;
+
+public class _0202_HappyNumber {
+
+	public static void main(String[] args) {
+		
+		// question from : https://leetcode.com/problems/happy-number/description/
+		System.out.println(isHappy(19)); // true
+		System.out.println(isHappy(2)); // false
+	}
+	
+	// Time complexity : O(log n)
+	// Space complexity : O (1)
+	public static boolean isHappy(int n) {
+        int slowRunner = n;
+        int fastRunner = getNext(n);
+
+        boolean isHappy = false;
+
+        // when fastRunner is equal to 1, then no need to get next
+        // when fastRunner is equal to slowRunner, then no need to get next
+        while(fastRunner!= 1 && fastRunner != slowRunner)
+        {
+            slowRunner = getNext(slowRunner);
+            fastRunner = getNext(getNext(fastRunner));
+        }
+
+        if(fastRunner == 1)
+        {
+        	isHappy = true;
+        }
+        
+        return isHappy;
+    }
+
+    public static int getNext(int n)
+    {
+        int total = 0;
+        
+        while(n > 0)
+        {
+            int remainder = n % 10;
+            n = n / 10;
+            total += remainder * remainder;
+        }
+        
+        return total;
+    }
+}
